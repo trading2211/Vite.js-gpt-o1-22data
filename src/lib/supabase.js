ba8900a4f7e8e9c69aa58406b9ef5cc9805d2fcb
trading2211 <117546269+trading2211@users.noreply.github.com>
2025-02-08 18:07:57 +0100

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ahfglshexgeplzdlkrlf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoZmdsc2hleGdlcGx6ZGxrcmxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyOTE4NTAsImV4cCI6MjA0NDg2Nzg1MH0.zg576a1SxYBqBizO8IsuiSkngNHg9fQ1WAfM4Dxz7Jg';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
